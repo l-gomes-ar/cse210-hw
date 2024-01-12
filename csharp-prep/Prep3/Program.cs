ac0ec18b9692d0bc0a8b9471167952eb155389c6
l-gomes-ar <lgaraujo021@gmail.com>
2024-01-12 20:46:02 +0000

    Prep3 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        do 
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess;
+
+            int countGuess = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                countGuess++;
+
+                if (guess < magicNumber) 
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (guess != magicNumber);
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Total number of guesses: {countGuess}");
+
+            Console.Write("Do you want to play again (yes/no)? ");
+            playAgain = Console.ReadLine();
+        } while (playAgain == "yes");
     }
 }
