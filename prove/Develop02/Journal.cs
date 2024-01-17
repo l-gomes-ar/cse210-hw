04eb54928f8cca5268ba6dd641a04bd2f677a455
l-gomes-ar <lgaraujo021@gmail.com>
2024-01-17 17:24:59 +0000

    'stubs' for classes added

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,28 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public Journal()
+    {
+    }
+
+    public void AddEntry(Entry newEntry)
+    {
+        //TODO
+    }
+
+    public void DisplayAll()
+    {
+        //TODO
+    }
+
+    public void SaveToFile(string file)
+    {
+        //TODO
+    }
+
+    public void LoadFromFile(string file)
+    {
+        // TODO
+    }
+}
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,15 @@
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+
+    public PromptGenerator()
+    {
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+
+}
