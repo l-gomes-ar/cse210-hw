1a6b06fb6786d24686988baa17b771941b820750
l-gomes-ar <lgaraujo021@gmail.com>
2024-01-30 20:59:43 +0000

    Learning04 Activity Submitted

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assign0 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assign0.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment assign1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assign1.GetSummary());
+        Console.WriteLine(assign1.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment assign2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign2.GetWritingInformation());
+
     }
 }
