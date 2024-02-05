f24b6eeda253c49cb88d7db2e45a7ae164482579
l-gomes-ar <lgaraujo021@gmail.com>
2024-02-05 21:10:18 +0000

    Prepare Activity 05 Added

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Step 5
+        Square square = new Square("Blue", 4);
+        Console.WriteLine($"Area: {square.GetArea()} Colour: {square.GetColor()}");
+
+        // Step 6
+        Rectangle rectangle = new Rectangle("Red", 5, 3);
+        Console.WriteLine($"Area: {rectangle.GetArea()} Colour: {rectangle.GetColor()}");
+
+        Circle circle = new Circle("Purple", 2.5);
+        Console.WriteLine($"Area: {circle.GetArea()} Colour: {circle.GetColor()}\n");
+
+        // Step 7
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());;
+        }
     }
 }
